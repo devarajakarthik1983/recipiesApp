59df91c32d8be2ae1c8872180d17ff249aca79a5
devarajakarthik83 <karthikdevaraja83@gmail.com>
2020-01-06 11:00:50 -0500

    Final Commit

---
+++ recipeApp/src/app/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
+
+@Injectable({
+    providedIn:'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService , private router:Router){}
+    canActivate(route :ActivatedRouteSnapshot , router: RouterStateSnapshot): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
+        return this.authService.user.pipe( take(1), map(user=>{
+           const auth = !!user;
+           if(auth) {
+               return true
+           } else {
+               return this.router.createUrlTree(['/auth'])
+           }
+        }))
+    }
+}
